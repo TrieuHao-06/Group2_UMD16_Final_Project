1457245e76d123d8138116f92458527c1db43892
TrieuHao-06 <haotrieu52@gmail.com>
2026-09-06 20:07:51 +0700

    Thêm dữ liệu tiến hành chạy file main_server.py

---
+++ Code/main_server.py
@@ -0,0 +1,17 @@
+# main_server.py
+from server.database import init_db
+from server.server import GameServer
+
+# Dòng này kích hoạt các lệnh dispatcher.register() bên trong api_routes.py
+import server.api_routes 
+
+if __name__ == "__main__":
+    print("========================================")
+    print("     KHỞI ĐỘNG SERVER CARO ONLINE       ")
+    print("========================================")
+    
+    init_db()
+    
+    server = GameServer()
+    server.start()
+
